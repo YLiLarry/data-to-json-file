67170677d7c1332e02f1b2f0097dc5ee8db8377f
Yu Li <YLiLarry@gmail.com>
2015-12-16 03:57:21 -0500

    init

---
+++ src/Data/SL.hs
@@ -0,0 +1,19 @@
+module Data.SL (module X, SL(..)) where
+
+import GHC.Generics as X (Generic(..))
+import Data.ByteString.Lazy as B (writeFile, readFile)
+import Data.Aeson as X (FromJSON(..), ToJSON(..), encode, eitherDecode)
+import Text.Printf (printf)
+
+class (FromJSON a, ToJSON a) => SL a where
+
+   save :: FilePath -> a -> IO ()
+   save path a = B.writeFile path (encode a)
+
+   load :: FilePath -> IO a
+   load path = do
+      result <- eitherDecode <$> B.readFile path
+      case result of
+         Left msg -> error $ printf "Error when reading the file \"%s\":\n%s" path msg
+         Right vl -> return vl
+
